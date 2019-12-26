ee10d688bee6f4954ed0f40827102df8752aeab7
“xyb1” <921866260@qq.com>
2019-12-26 19:05:58 +0800

    地方
    Signed-off-by: “xyb1” <921866260@qq.com>

---
+++ blog/src/App.vue
@@ -0,0 +1,29 @@
+
+ <transition
+ 	mode="out-in"
+ 	@before-enter="beforeEnter"
+ 	@enter="enter"
+ 
+ 	@before-leave="beforeLeave"
+ 	@leave="leave"
+ 	:css="false">
+ 
+ </transition> <template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './view/header.vue'
+export default {
+  name: 'app',
+  components: {
+   Header 
+  }
+}
+</script>
+
+<style>
+
+</style>
